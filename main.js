b8ee749757a6e8e615ff43707d470cf3a9fe26e0
Tarun Naik <78128450+TarunNaik-beep@users.noreply.github.com>
2021-12-09 16:50:30 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 song1 = "music.mp3";
 song2 = "music2.mp3";
 
+leftWristY = 0
+leftWristX = 0
+rightWristY = 0
+rightWristX = 0
+
+scoreLeftWrist = 0
+scoreRightWrist = 0
+
+song1_status = "";
+song2_status = "";
+
 function preload() {
     song_1 = loadSound("music.mp3");
     song_2 = loadSound("music2.mp3");
@@ -19,4 +30,36 @@ function setup() {
 
 function draw() {
     image(video,0,0,600,500);
+    fill("#FF0000");
+    stroke("FF0000");
+
+    song1_status = song1.isPlaying();
+    song2_status = song2.isPlaying();
+    if(scoreLeftWrist>0.2) {
+        circle(leftWristX,leftWristY,30);
+        song2.stop();
+        if(song1_status==false) {
+            song1.play();
+            document.getElementById("song_name").innerHTML = "Harry Potter is Playing";
+        }
+    }
+    if(scoreRightWrist>0.2) {
+        circle(rightWristX,rightWristY,30);
+        song1.stop();
+        if(song2_status==false) {
+            song2.play();
+            document.getElementById("song_name").innerHTML = "Peter Pan is Playing";
+        }
+    }
+}
+
+function gotposes(error, results) {
+    if(results.length) {
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+    }
 }
